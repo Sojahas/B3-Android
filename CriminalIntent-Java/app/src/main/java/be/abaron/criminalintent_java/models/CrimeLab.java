b7e330d16e5594e7724731365ed5be905fa45590
Sojahas <72076271+Sojahas@users.noreply.github.com>
2020-09-30 16:27:27 +0200

    3.1 Fait

---
+++ CriminalIntent-Java/app/src/main/java/be/abaron/criminalintent_java/models/CrimeLab.java
@@ -0,0 +1,45 @@
+package be.abaron.criminalintent_java.models;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+//Ajout d'une classe qui sera essentiellement une liste de crime avec un Design Pattern du singleton
+public class CrimeLab {
+    private static CrimeLab sCrimeLab; //préfixe s car les noms de champs statiques, selon la nomenclature, commencent par un s !
+    private List<Crime> mCrimes;
+
+    public static CrimeLab get() {
+        if (sCrimeLab == null) {
+            sCrimeLab = new CrimeLab();
+        }
+        return sCrimeLab;
+    }
+
+    public CrimeLab() {
+        mCrimes = new ArrayList<>();
+
+        //initialisation avec des crimes bidons
+        for (int i = 0; i < 100; i++) {
+            Crime crime = new Crime();
+            crime.setTitle("Crime #" + i);
+            crime.setSolved(i % 2 == 0); //On dit qu'une sur deux est résolue
+            mCrimes.add(crime);
+        }
+    }
+
+    public Crime getCrime(UUID id) {
+        for (Crime crime : mCrimes) {
+            if (crime.getID().equals(id))
+                return crime;
+        }
+        return null;
+    }
+
+    public List<Crime> getCrimes() {
+        //On fait un return Collections.unmodifiableList(mCrimes); dans le but de pouvoir
+        // renvoyer une vue non modifiable de la collection spécifiée, et une tentative de modification de la collection entraînera une UnsupportedOperationException.
+        return Collections.unmodifiableList(mCrimes);
+    }
+}
