9426a60f8c1f884985bce94d034964845a1c5e43
Sojahas <72076271+Sojahas@users.noreply.github.com>
2020-09-30 11:23:19 +0200

    2.5 Fait

--- CriminalIntent-Java/app/src/main/java/be/abaron/criminalintent_java/controllers/CrimeActivity.java
+++ CriminalIntent-Java/app/src/main/java/be/abaron/criminalintent_java/controllers/CrimeActivity.java
@@ -1,6 +1,8 @@
 package be.abaron.criminalintent_java.controllers;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
@@ -12,5 +14,15 @@ public class CrimeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager(); //La gestion des fragments, de leur ajout ou de leur retrait de la vue se fait grace à la classe FragmentManager
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container); //Demande de trouver le fragment hébérgé dans l'activité courante avec un vue avec l'id R.id.fragment_container
+        if (fragment == null) { //S'il n'y a pas encore de fragment chargé
+            fragment = new CrimeFragment(); //Alors on en crée un
+            //On l'ajoute en précisant l'identifiant du container hébergeur et le fragment à ajouter
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit(); //l'appel des commit va rendre effectif les changements
+        }
     }
 }
