d2f620b2599ef3382096f70640500282ce07454e
Sojahas <72076271+Sojahas@users.noreply.github.com>
2020-10-04 12:03:02 +0200

    Labo3 finit

--- CriminalIntent-Java/app/src/main/java/be/abaron/criminalintent_java/controllers/CrimeListFragment.java
+++ CriminalIntent-Java/app/src/main/java/be/abaron/criminalintent_java/controllers/CrimeListFragment.java
@@ -1,9 +1,12 @@
 package be.abaron.criminalintent_java.controllers;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -50,6 +53,42 @@ public class CrimeListFragment extends Fragment {
         }
     }
 
+    public class CrimeHolder extends RecyclerView.ViewHolder {
+        private Crime mCrime;
+        private TextView mTitleTextView;
+        private TextView mDateTextView;
+
+        public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.list_item_crime, parent, false));
+            mTitleTextView = (TextView) itemView.findViewById(R.id.crime_title);
+            mDateTextView = (TextView) itemView.findViewById(R.id.crime_date);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(getActivity(), CrimeActivity.class);
+                    intent.putExtra(CrimeFragment.CRIME_ID, mCrime.getID());
+                    startActivity(intent);
+                }
+            });
+        }
+
+        public void bind(Crime crime) {
+            mCrime = crime;
+            mTitleTextView.setText(crime.getTitle());
+            mDateTextView.setText(crime.getDate().toString());
+
+            if(mCrime.isSolved()){
+                mTitleTextView.setBackgroundColor(Color.GREEN);
+                mDateTextView.setBackgroundColor(Color.GREEN);
+            }
+            else {
+                mTitleTextView.setBackgroundColor(Color.RED);
+                mDateTextView.setBackgroundColor(Color.RED);
+            }
+        }
+    }
+
     //Utilise et instancie les ViewHolder
     private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder> {
 
@@ -80,4 +119,5 @@ public class CrimeListFragment extends Fragment {
             holder.bind(crime);
         }
     }
+
 }
